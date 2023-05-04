15d7097b23f49b14c1b6b70a4c623013e924a37c
gray-d <gray-d@delashoo.gray>
2023-05-04 17:07:11 +0300

    Updates004

--- contracts/fundme.sol
+++ contracts/fundme.sol
@@ -3,56 +3,78 @@ pragma solidity ^0.8.18;
 
 //IMPORTS
 import "./priceconverter.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-error NotOwner();
+//ERRORCODES
+error FUndMe__NotOwner();
 
+//Interfacess && Libraries >> Contracts
+
+/**
+ * @title A contract for crowd funding
+ * @author delashoo
+ * @notice This contract demos a sample funding contract
+ * @dev this implements pricefeeds as library
+ */
 contract FundMe {
+    //type declarations
     using priceconverter for uint256;
 
+    //state variables
     address public immutable i_owner; //gas efficiency
 
     AggregatorV3Interface public priceFeed;
 
-    constructor(address priceFeedAddress) {
-        i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
-    }
-    
     //Set minimum funding amount
     uint256 public constant MIN_USD = 25 * 1e18;
+
+    //mapping of addresses to amount they funded
+    mapping(address => uint256) AddressToAmount;
 
     //Array of addresses of funders
     address[] public funders;
 
-    //mapping of addresses to amount they funded
-    mapping (address => uint256) AddressToAmount;
-
-    modifier onlyOwner {
+    //modifiers
+    modifier onlyOwner() {
         //require(msg.sender == owner, "Sender is not owner!");
-        if(msg.sender != i_owner) {revert NotOwner();}
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
         _;
     }
 
-    function fund() public payable { 
-        require(msg.value.getConversionRate(priceFeed) >= MIN_USD, "Didn't send enough!"); //REVERTING
+    //FUNCTIONS
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+        //receive & fallbacK
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(priceFeed) >= MIN_USD,
+            "Didn't send enough!"
+        ); //REVERTING
         funders.push(msg.sender);
         AddressToAmount[msg.sender] += msg.value;
     }
-    /**
-     *Blockchain oracle - Any device that provides blockchains with external data & computation
-     * Chainlink - a modular decentralized oracle network to bring data & external computation.
-     can be completely customized to bring in any type of data/computation to blockchain network.
-     * Services include; 
-            1. DAtafeeds
-            2. Verifiable Random function
-            3. Chainlink Keepers - nodes that listen to specific events to perform actions.
-            4. End-to-end reliability for smart contracts. - 
-     */
 
     function withdraw() public payable onlyOwner {
         //for loop to loop thro' the array of funders removing the funders
         //starting index, ending index, step amount
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             AddressToAmount[funder] = 0;
         }
@@ -65,19 +87,23 @@ contract FundMe {
 
         //send
         //bool sendSuccess = payable(msg.sender).send(address(this).balance);
-       // require(sendSuccess, "Send failed");
+        // require(sendSuccess, "Send failed");
 
         //call - lower level command
-        (bool callSucces,) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSucces, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSucces, "call failed");
-    }
-
-    //receive & fallback
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
-    }
+    }    
 }
+
+/**
+     *Blockchain oracle - Any device that provides blockchains with external data & computation
+     * Chainlink - a modular decentralized oracle network to bring data & external computation.
+     can be completely customized to bring in any type of data/computation to blockchain network.
+     * Services include; 
+            1. DAtafeeds
+            2. Verifiable Random function
+            3. Chainlink Keepers - nodes that listen to specific events to perform actions.
+            4. End-to-end reliability for smart contracts. - 
+     */
